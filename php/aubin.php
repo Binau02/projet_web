2b910329c210bf65d83287dd0a2b36fab9ad7a9a
Aubin Saunier <aubin.saunier@gmail.com>
2023-06-26 10:28:32 +0200

    oui

---
+++ php/aubin.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html lang = "fr">
+
+  <head>
+    <meta charset = "utf-8">
+    <title>test aubin</title>
+
+    <link href="css/base.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <div>
+      
+    </div>
+  </body>
+
+</html>

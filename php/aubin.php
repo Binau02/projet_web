d36e5953f310ea1647889b83885a23d3ef3f4bc1
Aubin Saunier <aubin.saunier@gmail.com>
2023-06-26 11:54:16 +0200

    base.css + debut header

--- php/aubin.php
+++ php/aubin.php
@@ -6,12 +6,20 @@
     <meta charset = "utf-8">
     <title>test aubin</title>
 
-    <link href="css/base.css" rel="stylesheet">
+    <link href="../css/base.css" rel="stylesheet">
   </head>
 
   <body>
-    <div>
-      
+    <div id=header>
+      <div id=title>
+        <img id=logo src="../img/logo.png">
+        <span>BlaBlaCrash</span>
+      </div>
+      <form id=connexion action="connect.php" method="post">
+        <input type="email" placeholder="Email" aria-label="Search" name="email">
+        <input type="password" placeholder="Password" aria-label="Search" name="password">
+        <button type="submit" name="connect">Connect</button>
+      </form>
     </div>
   </body>
 
